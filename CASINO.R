fa6124b564b375980b62f3c3732a5e76d7409bfa
Comollo <comottofederico@gmail.com>
2018-06-28 12:16:35 +0200

    proposta comoto + modifiche

--- CASINO.R
+++ CASINO.R
@@ -198,4 +198,3 @@ Sistemato2 = d_libra %>%
 #spostamento tra le sezioni. Problema: perdiamo la serie storica perchè se è possibile che alcune
 #categorie di macchine, che presentano più di una macchina (num_macchine > 1) vengano divise tra 2
 #o più sezioni in uno stesso mese. Esempio lampante è la categoria 0.02 reel [vedete].
-
