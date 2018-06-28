d4c733760aec1212035f9621df95c246746a3b80
Eugenio_Andreose <andreose.eugenio@gmail.com>
2018-06-28 10:53:05 +0200

    fee_boh

--- CASINO.R
+++ CASINO.R
@@ -1,5 +1,5 @@
 #Caricamento Dataset 
-#setwd("C:\\Users\\Donatella\\Desktop\\bicocca\\decision models\\project\\progetto_finale")
+setwd("C:\\Users\\Donatella\\Desktop\\bicocca\\decision models\\project\\progetto_finale")
 library(readxl)
 d = read_excel("Lucky Duck Entertainment revenue 2013.xls", na = c(".", "NA", "NaN"))
 
@@ -15,11 +15,11 @@ summary(d$Month) #circa 12 mesi
 # CATEGORIZATION ##
 ###################
 
-#Per trovare il numero massimo di macchine allocabili in ciascun casinÃ²:
-#1) raggruppiamo per casinÃ² -> creo 2 dataset apposta;
+#Per trovare il numero massimo di macchine allocabili in ciascun casinç:
+#1) raggruppiamo per casinò -> creo 2 dataset apposta;
 #2) raggruppo per mese e sommo il numero di macchine;
-#3) considero il numero piÃ¹ alto: se quel mese ci sono state cosÃ¬ tante macchine allora possono starci sempre
-#il punto 3 che avevamo individuato al primo incontro forse ora Ã¨ inutile! Vogliamo un altro tipo di
+#3) considero il numero più alto: se quel mese ci sono state così tante macchine allora possono starci sempre
+#il punto 3 che avevamo individuato al primo incontro forse ora è inutile! Vogliamo un altro tipo di
 #categorizzazione
 
 d %>%
@@ -79,8 +79,11 @@ d_libra = d %>%
 
 status_libra = df_status(d_libra, F)
 
+library(lubridate)
+
+#Considero solo il mese di Settembre 
 sept_lib = d_libra %>%
-  filter (Month == as.Date("2011-09-01")) 
+  filter (Month == ymd("2011-09-01")) #Voi usate pure as.Date()
 
 sum(sept_lib$NoMachines) #numero macchine mese di settembre = 230 (Uguale al raggruppamento fatto al I incontro)
 
@@ -94,4 +97,104 @@ sept_lib %>%
 #giocate per macchina potrebbe essere il coin in? -> da MAX
 #profitto per macchina -> da MAX
 
+<<<<<<< HEAD
 
+=======
+#### 28/06/2018 #####
+### PROVA 2 #########
+#####################
+
+#suddivisione anche per sezioni...sempre mese di Settembre e casino Libra
+
+section_sept_lib = sept_lib %>% 
+  group_by(Denomination, MachineType, Section) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination, Section) #ok
+View(es)  
+
+#Considero solo il mese di Ottobre
+
+opt_lib = d_libra %>%
+  filter (Month == ymd("2011-10-01")) #Voi usate pure as.Date()
+
+sum(opt_lib$NoMachines) #numero macchine mese di settembre = 230 (Uguale al raggruppamento fatto al I incontro)
+
+opt_lib %>% 
+  group_by(Denomination, MachineType) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination) #ok
+
+#Si ottengono 12 categorie rispetto alle 14 presenti al mese di settembre, che ormai abbiamo
+#appurato essere quello con più valori[più veritiero]. Quindi anche in questo caso avremmo 
+#Missing Value  
+
+#suddivisione anche per sezioni...sempre mese di Ottobre e casino Libra
+section_opt_lib = opt_lib %>% 
+  group_by(Denomination, MachineType, Section) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination, Section) #ok
+
+#Considero solo il mese di Novembre
+
+nov_lib = d_libra %>%
+  filter (Month == ymd("2011-11-01")) #Voi usate pure as.Date()
+
+sum(nov_lib$NoMachines) #numero macchine mese di settembre = 230 (Uguale al raggruppamento fatto al I incontro)
+
+nov_lib %>% 
+  group_by(Denomination, MachineType) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination) #ok
+
+#Si ottengono 12 categorie rispetto alle 14 presenti al mese di settembre, che ormai abbiamo
+#appurato essere quello con più valori[più veritiero]. Quindi anche in questo caso avremmo 
+#Missing Value  
+
+#suddivisione anche per sezioni...sempre mese di Ottobre e casino Libra
+section_nov_lib = opt_lib %>% 
+  group_by(Denomination, MachineType, Section) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination, Section) #ok
+View(es)
+
+#Replica di quanto fatto in precedenza ma per tutti i mesi: ipotizzando che le categorie
+#siano 14 [guardo il mese di settembre - quello più completo], dovrei avere 14 * 12 = 168
+#osservazioni -> in realtà sono 147 [si ha però la serie storica a tutti gli effetti]
+
+Sistemato1 = d_libra %>% 
+  group_by(Denomination, MachineType, Month) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination) #ok
+
+#un'osservazione in questo dataset corrisponde ad una determinata categoria in un determinato mese
+#viene fornito il numero di macchine di quella categoria in quel mese e da lì si ricavano i valori
+#monetari -> bisogna aggregare anche forecast etc...
+
+#######################################
+
+Sistemato2 = d_libra %>% 
+  group_by(Denomination, MachineType, Section, Month) %>%
+  summarise(num_macchine = sum(NoMachines),
+            profitto_per_macchina = sum(GrossRevenue)/num_macchine, #profitto per macchina di quella categoria
+            giocate_per_macchina = sum(Plays)/num_macchine) %>% #giocata per macchina di quella categoria 
+  arrange(MachineType, Denomination, Section) #ok
+
+#un'osservazione corrisponde ad una determinata categoria in una determinata sezione del casino Libra,
+#questo dataset è strettamente dipendente a Sistemato1: guardate la categoria 0.01 reel. Ci sono
+#esattamente 8 osservazioni come in Sistemato1, ma in questo caso abbiamo informazioni circa lo
+#spostamento tra le sezioni. Problema: perdiamo la serie storica perchè se è possibile che alcune
+#categorie di macchine, che presentano più di una macchina (num_macchine > 1) vengano divise tra 2
+#o più sezioni in uno stesso mese. Esempio lampante è la categoria 0.02 reel [vedete].
+>>>>>>> 8c99f6280f201a2c18ade1cf6c67bddc811fd526
